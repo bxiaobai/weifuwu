b3bf1a99f89b050eb29661475952ba77123683b7
bug <16639579302@163.com>
2023-03-24 14:55:31 +0800

    登录login

--- src/main/java/com/example/loginserviceone/controller/LoginController.java
+++ src/main/java/com/example/loginserviceone/controller/LoginController.java
@@ -1,12 +1,30 @@
 package com.example.loginserviceone.controller;
 
 
+import com.example.commonutils.utils.CommonResult;
+import com.example.loginapi.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
+    UserService userService;
+
+    /**
+     * 登录请求
+     * @param name 姓名
+     * @param password 密码
+     * @return 0代表失败，1代表正确
+     */
+    @GetMapping("/user")
+    public CommonResult login(@RequestParam("name") String name , @RequestParam("password") String password){
+        return userService.login(name,password);
+    }
 
 }
--- src/main/java/com/example/loginserviceone/impl/UserServiceImpl.java
+++ src/main/java/com/example/loginserviceone/impl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.example.loginserviceone.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.commonutils.po.User;
+import com.example.commonutils.utils.CommonResult;
 import com.example.loginapi.api.UserService;
 import com.example.loginserviceone.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.UUID;
 
 /**
 * @author Administrator
@@ -12,9 +20,38 @@ import org.springframework.stereotype.Service;
 * @createDate 2023-03-24 14:00:24
 */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Resource
+    UserMapper userMapper;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Override
+    public CommonResult login(String name, String password) {
+
+        log.info("登录请求进行中..........................");
+        User user = query().eq("name", name).one();
+
+
+        if (user == null){
+            throw new RuntimeException("账号不存在!");
+        }
+
+
+        if (!user.getPassword().equals(password)){
+            throw new RuntimeException("密码错误!");
+        }
+
+        log.info("登录验证结束，登录成功..........................");
+
+
+        return CommonResult.success(user);
+    }
 }
 
 
